831d3ce1801106cf4e29d198c6abfed02197cd33
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-12-04 18:55:39 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write a program to perform the following operations on a List: 
+# 1. Create an empty list 
+# 2. A list with elements 
+# 3. Use * operator 
+# 4. Reverse a list
+
+a=["Ashleyna", 15, 3, 12]
+print(len(a))
+print(a[2:4])
+print(a*3)
+a.reverse()
+print(a)
